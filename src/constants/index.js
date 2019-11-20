d5cc652a8bc8f7d1aea39d01a435f04fae295347
soncomqiq <sonter@buzzfreeze.com>
2019-11-20 22:28:06 +0700

    edit production code

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://localhost:8080/api';
+export const API_BASE_URL = (process.env.NODE_ENV !== 'production') ? '//staging.localhost:8000' : 'http://localhost:8080/api';
 export const ACCESS_TOKEN = 'accessToken';
 
 export const NAME_MIN_LENGTH = 4;
